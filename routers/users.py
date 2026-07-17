c763ee7b56942b01dbefd63b50695e4b600a84c3
Sufyan240403 <sufyan240403@gmail.com>
2026-07-17 22:57:33 +0100

    Add users.py - register and login endpoints

--- routers/users.py
+++ routers/users.py
@@ -22,19 +22,9 @@ def register (user: UserCreate, db:Session= Depends (get_db)):
     db.refresh(new_user)
     return {"message": "user created successfully"}
 
+
 @router.post("/login")
-def login(user, User_create, db = Session.Depends(get_db)):
-    existing_user =db.query(user).filter(user.email == user.email).first()
-    if not existing_user:
-        raise HTTPException(staus_code = 401, detail ="user not found)")
-        hashed_password = db.query(hashed_password).filter(hashed_password == hashed_password).first()
-        if not hashed_password:
-            raise HTTPException(status_code=401, detail="invalid email or password")
-            create_access_token
-            return{"message": "login successfull", "access_token": create_access_token}
-        
-@router.post("/users")
-def users(user: UserCreate, db: Session = Depends(get_db)):
+def login(user: UserCreate, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.email == user.email).first()
     if not db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
